e06298279c0eebaac0ed7c5fe072b3f9e9c3828f
NovaDevSas <novadevsas@gmail.com>
2024-12-17 17:31:25 -0500

    Wow

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,38 @@
-// src/controllers/userController.js
 const User = require('../models/User');
 
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find().select('-password'); // Excluir el campo de la contraseña
+    const users = await User.find();
     res.json(users);
   } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Usuario eliminado' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
-// src/routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/users', userController.getUsers); // Ruta para obtener la lista de usuarios
+router.post('/users', userController.createUser); // Ruta para crear un nuevo usuario
+router.put('/users/:id', userController.updateUser); // Ruta para actualizar un usuario
+router.delete('/users/:id', userController.deleteUser); // Ruta para eliminar un usuario
 
 module.exports = router;
